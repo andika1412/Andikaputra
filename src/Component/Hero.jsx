4b189a695b4b78a562d2e699c2edecb3d121cf97
andika <andikadwiputra1412@gmail.com>
2024-01-20 00:30:17 +0700

    penambahan gambar

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
               </div>
             </div>
             <div className=" w-full  xl:w-1/2 ">
-              <img src="./Andikaputra/public/Images/Hero.png" alt="" className="mx-auto pb-[32px]" />
+              <img src="./ANDIKA/public/Images/Hero.png" alt="" className="mx-auto pb-[32px]" />
             </div>
           </div>
         </div>
