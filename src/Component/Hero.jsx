119e29329159908bfee11a246341967b247b52d5
andika1412 <97074481+andika1412@users.noreply.github.com>
2024-01-20 16:51:02 +0700

    Update Hero.jsx

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Hero from ".../public/Images/Hero.png";
 
 const Hero = () => {
   return (
@@ -38,7 +37,7 @@ const Hero = () => {
               </div>
             </div>
             <div className=" w-full  xl:w-1/2 ">
-              <img src={Hero} alt="" className="mx-auto pb-[32px]" />
+              <img src="../Andikaputra/Hero.png" alt="" className="mx-auto pb-[32px]" />
             </div>
           </div>
         </div>
